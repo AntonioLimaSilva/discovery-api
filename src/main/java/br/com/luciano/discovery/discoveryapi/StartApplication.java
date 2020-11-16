4595ef01ac09689e45c817b4ebf570b2f566899c
Luciano Lima <luclimasilva@hotmail.com>
2020-11-16 18:39:26 -0300

    Initial commit

---
+++ src/main/java/br/com/luciano/discovery/discoveryapi/StartApplication.java
@@ -0,0 +1,15 @@
+package br.com.luciano.discovery.discoveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
